e7d03607a4c86d9f1eed2f4858efbbe1481de36f
xb <xb24407@126.com>
2016-03-03 18:38:43 +0800

    add files

---
+++ example/src/main/java/com/app/model/Student.java
@@ -0,0 +1,36 @@
+package com.app.model;
+
+/**
+ * Created by xiaojunzhou on 2016/3/1.
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
---
+++ example/src/main/java/com/app/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.app.service;
+
+import com.app.model.Student;
+
+/**
+ * Created by xiaojunzhou on 2016/3/1.
+ */
+public interface StudentService {
+    int addStudent(Student student);
+}
